9d4ecb99814c756ffd832568d2063f56633bb405
VAnastasiaC <v.anastasia.c@gmail.com>
2026-05-06 16:39:48 +0100

    Add service page all

--- vite.config.js
+++ vite.config.js
@@ -34,7 +34,7 @@ export default defineConfig(() => {
             rollupOptions: {
                 input: {
                     main: 'index.html',
-                    login: 'register.html',
+                    login: 'service.html',
                 },
             },
         },
